4c6aed584273816331a147a34e62fe239e2d56ea
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-08-25 14:12:10 -0300

    created project

---
+++ src/test/java/com/rvpds/restwithspringbootandjavaerudio/RestWithSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvpds.restwithspringbootandjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
